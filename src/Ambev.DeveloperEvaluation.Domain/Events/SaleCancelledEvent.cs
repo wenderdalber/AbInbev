0396e60f1d65974b8a52019791af44692847324c
wenderdalber <wenderdalber@gmail.com>
2025-02-12 21:36:35 -0300

    implement events for order and unit tests

---
+++ src/Ambev.DeveloperEvaluation.Domain/Events/SaleCancelledEvent.cs
@@ -0,0 +1,22 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.Domain.Events;
+
+/// <summary>
+/// Event triggered when a sale is cancelled.
+/// </summary>
+public class SaleCancelledEvent : INotification
+{
+    public Order Order { get; }
+
+    public SaleCancelledEvent(Order order)
+    {
+        Order = order;
+    }
+}
